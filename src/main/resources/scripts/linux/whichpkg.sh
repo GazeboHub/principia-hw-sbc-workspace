da55d96613645837b55bd3f96d1a687722715339
Sean Champ <spchamp+git-muletop@me.com>
2014-04-14 13:27:54 -0700

    update for the update-alternatives instance

--- src/main/resources/scripts/linux/whichpkg.sh
+++ src/main/resources/scripts/linux/whichpkg.sh
@@ -2,9 +2,34 @@
 #
 # whichpkg.sh - determine which Debian package provides a shell command
 #
-# version 1.0
+# version 1.1
 #
 # dependencies:
+#  deb:coreutils
 #  deb:dpkg
+#  deb:gawk
+#  deb:grep
 
-exec dpkg-query -S $(which $1)
+
+do_query() {
+        dpkg-query -S $1 2>/dev/null
+}
+
+print_pkg() {
+        echo $1 | cut -d: -f1
+}
+
+
+P="$(which $1)"
+PKG=$(do_query $P)
+
+if [ -n "${PKG}" ]; then
+    print_pkg "${PKG}"
+    exit 0
+else
+    ## assume the command was installed cf. `update-alternatives`
+    P_ACTUAL=$(update-alternatives --query "$1" |
+                grep '^Value:' | awk '{print $2}')
+    PKG_ACTUAL=$(do_query $P_ACTUAL)
+    print_pkg "${PKG_ACTUAL}"
+fi
